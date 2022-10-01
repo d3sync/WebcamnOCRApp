5adec1a3477924aa16f20cfe576e69315e6bfcc2
d3sync <alex@idle.gr>
2022-10-01 21:29:42 +0300

    Add project files.

---
+++ WebcamnOCRApp/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿
+namespace WebcamnOCRApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cboWebcam = new System.Windows.Forms.ComboBox();
+            this.pic = new System.Windows.Forms.PictureBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.picSnapped = new System.Windows.Forms.PictureBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picSnapped)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cboWebcam
+            // 
+            this.cboWebcam.FormattingEnabled = true;
+            this.cboWebcam.Location = new System.Drawing.Point(12, 21);
+            this.cboWebcam.Name = "cboWebcam";
+            this.cboWebcam.Size = new System.Drawing.Size(507, 21);
+            this.cboWebcam.TabIndex = 0;
+            // 
+            // pic
+            // 
+            this.pic.Location = new System.Drawing.Point(12, 48);
+            this.pic.Name = "pic";
+            this.pic.Size = new System.Drawing.Size(506, 341);
+            this.pic.TabIndex = 1;
+            this.pic.TabStop = false;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(44, 395);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(58, 43);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(108, 395);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(58, 43);
+            this.btnStop.TabIndex = 3;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(460, 395);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(58, 43);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Snap";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // picSnapped
+            // 
+            this.picSnapped.Location = new System.Drawing.Point(524, 48);
+            this.picSnapped.Name = "picSnapped";
+            this.picSnapped.Size = new System.Drawing.Size(512, 341);
+            this.picSnapped.TabIndex = 5;
+            this.picSnapped.TabStop = false;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(6, 442);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(1029, 160);
+            this.richTextBox1.TabIndex = 6;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1048, 608);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.picSnapped);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.pic);
+            this.Controls.Add(this.cboWebcam);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picSnapped)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cboWebcam;
+        private System.Windows.Forms.PictureBox pic;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox picSnapped;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ WebcamnOCRApp/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using IronOcr;
+
+namespace WebcamnOCRApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        FilterInfoCollection filterinfoCol;
+        VideoCaptureDevice videocptdev;
+
+        public TesseractConfiguration Configuration { get; private set; }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //WebCamSample.WebCamDialog dlg = new WebCamSample.WebCamDialog();
+            //dlg.ShowDialog();
+            filterinfoCol = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo filterinfo in filterinfoCol)
+                cboWebcam.Items.Add(filterinfo.Name);
+            cboWebcam.SelectedIndex= 0;
+            videocptdev = new VideoCaptureDevice();
+
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            videocptdev = new VideoCaptureDevice(filterinfoCol[cboWebcam.SelectedIndex].MonikerString);
+            videocptdev.NewFrame += Videocptdev_NewFrame;
+            videocptdev.Start();
+        }
+
+        private void Videocptdev_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            pic.Image = (Bitmap)eventArgs.Frame.Clone();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (videocptdev.IsRunning == true)
+                videocptdev.Stop();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            if (videocptdev.IsRunning == true)
+                videocptdev.Stop();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            picSnapped.Image = pic.Image;
+            var Ocr = new IronTesseract();
+            using (var Input = new OcrInput(picSnapped.Image))
+            {
+                //Ocr.Language = OcrLanguage.EnglishBest;
+                //Input.Deskew();  // use if image not straight
+                //Input.DeNoise(); // use if image contains digital noise
+                Ocr.Language = OcrLanguage.English;
+                Ocr.AddSecondaryLanguage(OcrLanguage.Greek);
+                Configuration = new TesseractConfiguration()
+                {
+                    ReadBarCodes = true,
+                    BlackListCharacters = "`ë|^",
+                    RenderSearchablePdfsAndHocr = true,
+                    PageSegmentationMode = TesseractPageSegmentationMode.AutoOsd,
+                };
+                Input.Deskew();
+                Input.DeNoise();
+                Input.Dilate();
+                //Input.EnhanceResolution(300);
+                var Result = Ocr.Read(Input);
+                Console.WriteLine(Result.Text);
+                richTextBox1.Clear();
+                richTextBox1.Text = Result.Text;
+            }
+        }
+    }
+}
